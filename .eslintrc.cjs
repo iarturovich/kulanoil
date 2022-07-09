56939fe9e0e893db40e0644c9ef0e2624281813f
iarturovich <ilyas.arturovich@gmail.com>
2022-07-09 12:12:13 +0600

    setting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,11 +1,99 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
-
 module.exports = {
-  "root": true,
-  "extends": [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier"
+  root: true,
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended'
+  ],
+  env: {
+    'vue/setup-compiler-macros': true
+  },
+  rules: {
+    'no-useless-escape': 'off',
+    'comma-dangle': ['error', 'never'],
+    quotes: ['error', 'single'],
+    'no-multiple-empty-lines': [
+      'error',
+      {
+        max: 1,
+        maxEOF: 0
+      }
+    ],
+    semi: ['error', 'never'],
+    'space-infix-ops': ['error'],
+    'object-curly-spacing': ['error', 'always'],
+    'no-multi-spaces': ['error'],
+    'key-spacing': [
+      2,
+      {
+        beforeColon: false,
+        afterColon: true
+      }
+    ],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'no-trailing-spaces': 'error',
+    'padded-blocks': [
+      'error',
+      {
+        blocks: 'never'
+      }
+    ],
+    'no-tab': 'off',
+    'vue/html-closing-bracket-newline': [
+      'error',
+      {
+        multiline: 'never'
+      }
+    ],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: {
+          max: 1
+        },
+        multiline: {
+          max: 1
+        }
+      }
+    ],
+    'vue/first-attribute-linebreak': [
+      'error',
+      {
+        singleline: 'beside',
+        multiline: 'below'
+      }
+    ],
+    'vue/v-on-event-hyphenation': [
+      'warn',
+      'never',
+      {
+        autofix: true
+      }
+    ],
+    'vue/attribute-hyphenation': ['error', 'never'],
+    'vue/require-explicit-emits': 'off',
+    'vue/no-template-shadow': 'off',
+    'vue/padding-line-between-blocks': ['error', 'always'],
+    'vue/html-indent': [
+      'error',
+      2,
+      {
+        attribute: 1,
+        baseIndent: 1,
+        closeBracket: 0,
+        alignAttributesVertically: true,
+        ignores: []
+      }
+    ],
+    'vue/require-default-prop': 'off'
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.spec.{js,ts,jsx,tsx}',
+        'cypress/integration/**.spec.{js,ts,jsx,tsx}'
+      ],
+      extends: ['plugin:cypress/recommended']
+    }
   ]
 }
